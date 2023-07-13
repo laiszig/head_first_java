eec0ac9665fefa61cc24560213eff9f4818ce1b1
lais <laisziegler@gmail.com>
2023-07-13 19:16:45 -0300

    compiled classes

---
+++ Chapter_1/PoolPuzzleOne.java
@@ -0,0 +1,47 @@
+class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+            System.out.print("a");
+            if(x < 1) {
+            System.out.print(" ");
+            }   
+            System.out.print("n");
+            if(x > 1) {
+            System.out.print("noys");
+            x = x + 1;
+            }   
+            if (x == 1) {
+            System.out.print("oise");
+            } 
+            if (x > 3) {
+                System.out.print(" oyster");
+            }
+            System.out.println();
+            x = x - 1;
+        }
+    }
+}
+
+
+
+/*
+a noise
+annoys
+an oyster
+*/
+
+// For each line, I need to go through the loop once.
+/*
+System.out.print(" ");          x > 0
+System.out.print("a");          x < 1
+System.out.print("n");          x > 1
+System.out.print("an);          x > 3
+.                               x < 4
+x = x + 1                       System.out.print("noys");
+x = x + 2                       System.out.print("oise");
+x = x - 2                       System.out.print(" oyster");
+x = x - 1                       System.out.print("annoys");
+.                               System.out.print("noise");
+*/
