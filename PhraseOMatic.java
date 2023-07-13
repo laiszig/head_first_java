b2e28b56e6e9a2dd138a071e21a0c8a860f5ef2a
lais <laisziegler@gmail.com>
2023-07-13 09:54:23 -0300

    exercises unit 1

---
+++ PhraseOMatic.java
@@ -0,0 +1,27 @@
+public class PhraseOMatic {
+
+    public static void main(String[] args) {
+
+        String[] wordListOne = {"agnostic", "opinionated", "voice activated", "haptically driven", "extensible",
+                "reactive", "agent based", "functional", "AI enabled", "strongly typed"};
+
+        String[] wordListTwo = {"loosely coupled", "six sigma", "asynchronous", "event driven", "pub-sub",
+                "IoT", "cloud native", "service oriented", "containerized", "serverless", "microservices", "distributed ledger"};
+
+        String[] wordListThree = {"framework", "library", "DSL", "REST API", "repository", "pipeline",
+        "service mesh", "architecture", "perspective", "design", "orientation"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        java.util.Random randomGen = new java.util.Random();
+        int rand1 = randomGen.nextInt(oneLength);
+        int rand2 = randomGen.nextInt(twoLength);
+        int rand3 = randomGen.nextInt(threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("What we need is a " + phrase);
+
+    }
+}
